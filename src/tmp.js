de9b7db97ed50bfebd88eb726191e02062e7b39a
Joe Hachem <joehachem2000@gmail.com>
2021-05-20 19:47:31 +0300

    done

--- src/tmp.js
+++
@@ -1,6 +0,0 @@
-// clientId = '6499b09693a64a09ae4d961182500473'
-// clientSecret = 'd5b191d342e843f0af8520273e11a4b1'
-
-// https://accounts.spotify.com/authorize
-// GET https://api.spotify.com/v1/search
-// GET https://api.spotify.com/v1/artists/{id}/albums
